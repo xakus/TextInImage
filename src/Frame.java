97a417aaa34a6e03e1273a81820b3c791c9ebdee
xakus <xakus22@gmail.com>
2016-03-16 11:22:28 +0400

    Добавлена форма и чтение изображения на форму

---
+++ src/Frame.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+/**
+ * Created by xakus on 10.03.2016.
+ */
+public class Frame extends JFrame {
+    public Frame() {
+        this.setSize(200,50);
+        this.setResizable(false);
+        this.setLocation(0,0);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+    public int getLocationX(){
+        return (int) this.getLocation().getX();
+    }
+    public int getLocationY(){
+        return (int) this.getLocation().getY();
+    }
+    public void visible(){
+        this.setVisible(true);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,84 @@
-import com.aspose.ocr.ImageStream;
-import com.aspose.ocr.OcrEngine;
+//import com.aspose.ocr.ImageStream;
+//import com.aspose.ocr.OcrEngine;
+//
+///**
+// * Created by xakus on 10.03.2016.
+// * lib взял на сайтн http://www.aspose.com/
+// */
+//public class Main {
+// public  static  void main(String[] args) {
+//     //Initialize an instance of OcrEngine
+//     OcrEngine ocrEngine = new OcrEngine();
+//
+////Set the Image property by loading the image from file path location
+//     ocrEngine.setImage(ImageStream.fromFile("C:\\Users\\xakus\\Desktop\\hqdefault.jpg"));
+//
+////Process the image
+//     if (ocrEngine.process()) {
+//         //Display the recognized text
+//         System.out.println(ocrEngine.getText());
+//     }
+// }
+//}
 
-/**
- * Created by xakus on 10.03.2016.
- * lib взял на сайтн http://www.aspose.com/
- */
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+import javax.swing.filechooser.FileSystemView;
+
 public class Main {
- public  static  void main(String[] args) {
-     //Initialize an instance of OcrEngine
-     OcrEngine ocrEngine = new OcrEngine();
+    JLabel jLabel = new JLabel();
+    Frame frame = new Frame();
+    BufferedImage bufferedImage;
+    public Main() {
 
-//Set the Image property by loading the image from file path location
-     ocrEngine.setImage(ImageStream.fromFile("C:\\Users\\xakus\\Desktop\\hqdefault.jpg"));
 
-//Process the image
-     if (ocrEngine.process()) {
-         //Display the recognized text
-         System.out.println(ocrEngine.getText());
-     }
- }
+
+        frame.add(jLabel);
+        frame.visible();
+        final ImageIcon[] imageIcon = {null};
+        Thread thread=new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true){
+                    bufferedImage = grabScreen(frame.getLocationX()+4, frame.getLocationY()+32, 200, 50);
+                    imageIcon[0] = new ImageIcon( bufferedImage);
+                    jLabel.setIcon(imageIcon[0]);
+                }
+            }
+        } );
+      thread.start();
+    }
+
+
+
+
+    public static void main(String[] args) {
+        try {
+            //ImageIO.write(grabScreen(), "png", new File(getHomeDir(), "screen.png"));
+            new Main();
+        } catch (Exception e) {
+            System.out.println("IO exception" + e);
+        }
+    }
+
+    private static File getHomeDir() {
+        FileSystemView fsv = FileSystemView.getFileSystemView();
+        return fsv.getHomeDirectory();
+    }
+
+    private static BufferedImage grabScreen(int x1, int x2, int y1, int y2) {
+        try {
+            return new Robot().createScreenCapture(new Rectangle(x1, x2, y1, y2));
+        } catch (SecurityException e) {
+        } catch (AWTException e) {
+        }
+        return null;
+    }
+
 }
